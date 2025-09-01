782b90c76d11d17c7bbda2adbe2f7a55fe24b994
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-01 14:11:31 -0300

    011

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // --------------------------------
+        // GATES -=> Gate é um mecanismo de autorização — serve para verificar se o usuário autenticado pode ou não realizar uma determinada ação.
+        // --------------------------------
+
+        // Define a gate that checks if the user is admin
+        // Gate::define('admin', function () {
+        //     aqui esta dando erro
+        //     return auth()->user()->role === 'admin'; 
+        // });
+        Gate::define('admin', function ($user) {
+            return $user->role === 'admin';
+        });
     }
 }
