9259a5d9b86edd52fc53ca6e0461ec69caa32171
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-01 13:14:56 -0300

    10.1

--- app/Providers/FortifyServiceProvider.php
+++ app/Providers/FortifyServiceProvider.php
@@ -54,5 +54,10 @@ class FortifyServiceProvider extends ServiceProvider
         Fortify::requestPasswordResetLinkView(function () {
             return view('auth.forgot-password');
         });
+
+        // reset password view
+        Fortify::resetPasswordView(function ($request) {
+            return view('auth.reset-password', ['token' => $request->route('token')]);
+        });
     }
 }
