00a48f3bbc46d966149cac616dbae5fd449cf578
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-02 14:01:38 -0300

    016

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -4,15 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DepartmentController extends Controller
 {
     public function index()
     {
+        Auth::user()->can('admin') ?: abort(403, 'You are not authorized to acess this page'); // erro mensage
 
+        $departments = Department::all(); // will look for all deparments that is authorizate
 
-        $departments = Department::all();
-
-        return view('department.departments', compact('departments'));
+        return view('department.departments', compact('departments')); // will return to the page 'departments'
     }
 }
