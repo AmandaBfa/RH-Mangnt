55745852035d9c5a4eb8c42288826fc7dfcff684
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-03 15:28:22 -0300

    023

--- resources/views/colaborators/rh-users.blade.php
+++ resources/views/colaborators/rh-users.blade.php
@@ -33,7 +33,7 @@
                                 $permissions = json_decode($colaborator->permissions);
                             @endphp
 
-                            <td>{{ implode($permissions, ',') }}</td>
+                            <td>{{ implode(',', $permissions) }}</td>
 
                             <td>
 
