b7af68085d2525d4d97724d9e61ab8894d872ed4
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-08 16:18:59 -0300

    035

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -11,6 +12,8 @@ class User extends Authenticatable
 {
     // esta relacionado ao envio do email
     use Notifiable;
+
+    use SoftDeletes;
 
     public function detail()
     {
--- routes/web.php
+++ routes/web.php
@@ -50,4 +50,8 @@ Route::middleware('auth')->group(function () {
     // ------------ ADMIN COLABORATORS LIST ------------
     Route::get('/colaborators', [ColaboratorsController::class, 'index'])->name('colaborators.all-colaborators');
     Route::get('/colaborators/details/{id}', [ColaboratorsController::class, 'showDetails'])->name('colaborators.details');
+
+    // ------------ DELETE ADMIN COLABORATORS ------------
+    Route::get('/colaborators/delete/{id}', [ColaboratorsController::class, 'deleteColaborator'])->name('colaborators.delete');
+    Route::get('/colaborators/delete-confirm/{id}', [ColaboratorsController::class, 'deleteColaboratorConfirm'])->name('colaborators.delete-confirm');
 });
