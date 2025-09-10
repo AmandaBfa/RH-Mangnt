0eb0523836b74060da547e53c827b65041e403b1
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-10 15:47:40 -0300

    044

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,8 @@ Route::middleware('auth')->group(function () {
     Route::post('/rh-users/management/create-colaborator', [RhManagementController::class, 'createColaborator'])->name('rh.management.create-colaborator');
     Route::get('/rh-users/management/edit-colaborator/{id}', [RhManagementController::class, 'editColaborator'])->name('rh.management.edit-colaborator');
     Route::post('/rh-users/management/update-colaborator', [RhManagementController::class, 'updateColaborator'])->name('rh.management.update-colaborator');
+    Route::get('/rh-users/management/details/{id}', [RhManagementController::class, 'showDetails'])->name('rh.management.details');
+
 
     // ------------ ADMIN COLABORATORS LIST ------------
     Route::get('/colaborators', [ColaboratorsController::class, 'index'])->name('colaborators.all-colaborators');
