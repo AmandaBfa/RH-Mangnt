6f21245dfae4876af5849485227ab1c6f58b7cbf
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-11-12 15:39:14 -0300

    tests_015

---
+++ app/Services/GeneralServices.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Process\FakeProcessResult;
+
+class generalServices
+{
+    public static function checkIfSalaryIsGreaterThan($salary, $amount)
+    {
+        return $salary > $amount;
+    }
+
+    public static function createPhraseWithNameAndSalary($name, $salary)
+    {
+        return "O salário do(a) $name é $salary";
+    }
+
+    public static function getSalaryWithBonus($salary, $bonus)
+    {
+        return $salary + $bonus;
+    }
+}
