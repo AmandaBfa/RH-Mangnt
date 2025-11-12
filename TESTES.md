139d015eb30f5d652e053a31c85f77cc3c172d27
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-11-12 14:36:04 -0300

    tests_010

--- TESTES.md
+++ TESTES.md
@@ -10,7 +10,8 @@
 > > -   testar processo de login com sucesso para o perfil de rh
 > > -   testar processo de login com sucesso para o perfil de colaborador
 
--   testar a inserção de um novo user rh com o perfil do admin
+> > -   testar a inserção de um novo user rh com o perfil do admin
+
 -   testar a inserção de um novo colaborador com o perfil do rh
 
 -   testar se um admin, apos login com sucesso, consegue ver a pagina de colaboradores rh
--- tests/Feature/CreateUserTest.php
+++ tests/Feature/CreateUserTest.php
@@ -39,13 +39,54 @@ it('tests if an admin can insert a new Rh user', function () {
         'permissions' => '["rh"]',
     ]);
 
-    // verifica se o user rh foi inserido com sucesso
-    $this->assertDatabaseHas('users', [
-        'name' => 'RH user 1',
-        'email' => 'rhuser@gmail.com',
-        'role' => 'rh',
-        'permissions' => '["rh"]',
+    // // verifica se o user rh foi inserido com sucesso
+    // $this->assertDatabaseHas('users', [
+    //     'name' => 'RH user 1',
+    //     'email' => 'rhuser@gmail.com',
+    //     'role' => 'rh',
+    //     'permissions' => '["rh"]',
+    // ]);
+    expect(User::where('email', 'rhuser@gmail.com'));
+});
+
+it('tests if an RH User can insert a new Colaborator User', function () {
+    // criar user rh
+    addRhUser(); // por estar na função teste, ele tambem esta disponivel nesse arquivo
+
+    // criar os departamentos
+    addDepartment('Administração');
+    addDepartment('Recursos Humanos');
+    addDepartment('Armazém');
+
+    // login com o RH
+    $this->post('/login', [
+        'email' => 'rh1@rhmangnt.com',
+        'password' => 'Aa123456'
     ]);
+
+    // verifica se o login foi feito com sucesso
+    expect(auth()->user()->role)->toBe('rh');
+
+    // verifica se o admin consegue adicionar user de rh
+    $result = $this->post('/rh-users/management/create-colaborator', [
+        'name' => 'Colaborator 1',
+        'email' => 'colaborator1@gmail.com',
+        'select_department' => 3,
+        'address' => 'Rua 2',
+        'zip_code' => '1234-000',
+        'city' => 'City 2',
+        'phone' => '123456789',
+        'salary' => '1000.00',
+        'admission_date' => "2025-06-09",
+        'role' => 'colaborator',
+        'permissions' => '["colaborator"]'
+    ]);
+
+    // verifica se o user rh foi inserido com sucesso
+    // $this->assertDatabaseHas('users', [
+    //     'email' => 'colaborator1@gmail.com',
+    // ]);
+    expect(User::where('email', 'colaborator1@gmail.com'));
 });
 
 function addDepartment($name)
