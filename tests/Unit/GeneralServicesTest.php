2ababa4ea572286efb717a6bddbba9016e64ee1a
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-11-12 16:20:16 -0300

    tests_018

--- tests/Unit/GeneralServicesTest.php
+++ tests/Unit/GeneralServicesTest.php
@@ -29,14 +29,14 @@ it('Tests if the phrase is created correctly', function () {
     expect($result)->toBe('O salário do(a) João Ribeiro é 1000');
 });
 
-// it('Tests if the salary with bonus is calculater correctly', function () {
-//     $salary = 1000;
-//     $bonus = 250;
+it('Tests if the salary with bonus is calculater correctly', function () {
+    $salary = 1000;
+    $bonus = 250;
 
-//     $result = generalServices::getSalaryWithBonus($salary, $bonus);
+    $result = generalServices::getSalaryWithBonus($salary, $bonus);
 
-//     expect($result)->toBe(1250);
-// });
+    expect($result)->toBe(1250);
+});
 
 // it('Tests if the fake json data is created correctly', function () {
 
